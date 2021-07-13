14cb6ee940fcfe380a085a7a07684dc119c82b08
Rom0105 <savosko.photo@gmail.com>
2021-07-13 22:31:03 +0300

    правки

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   "Зелень",
   "Приправы",
 ];
+
+const ulIngredientsEl = document.querySelector("#ingredients");
+
+const element = ingredients.map((option) => {
+  const ingredientsEl = document.createElement("li");
+  ingredientsEl.textContent = option;
+  return ingredientsEl;
+});
+
+ulIngredientsEl.append(...element);
