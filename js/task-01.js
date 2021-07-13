a4b6e7763fac12eaf3628509b9f445448c481fe9
Rom0105 <savosko.photo@gmail.com>
2021-07-13 14:43:32 +0300

    правки

--- js/task-01.js
+++ js/task-01.js
@@ -1,2 +1,9 @@
-const itemEl = document.querySelectorAll(".item");
+const itemEl = document.querySelectorAll("h2");
 console.log(`В списке ${itemEl.length} категории.`);
+
+itemEl.forEach((item) => {
+  console.log(`Категория: ${item.textContent}`);
+  console.log(
+    `Количество элементов: ${item.nextElementSibling.children.length}`
+  );
+});
